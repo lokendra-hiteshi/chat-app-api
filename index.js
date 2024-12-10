6badf519ceabe8aee47f09c20b5a5b721f8ab9a3
lokendra-hitshi <lokendra.patidar@hiteshi.com>
2024-12-10 15:18:14 +0530

    Break code in seprate file like controllers and db

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
-const { Pool } = require("pg");
-const bcrypt = require("bcrypt");
+const db = require("./controller");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
+const { pool } = require("./db");
 
 dotenv.config();
 
@@ -16,14 +16,6 @@ const io = new Server(server, {
     origin: "*",
     methods: ["GET", "POST"],
   },
-});
-
-const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: process.env.DB,
-  password: process.env.DB_PASS,
-  port: process.env.POSTGRES_PORT,
 });
 
 app.use(
@@ -47,161 +39,16 @@ function authenticateToken(req, res, next) {
   });
 }
 
-app.get("/rooms", authenticateToken, async (req, res) => {
-  try {
-    const result = await pool.query("SELECT * FROM rooms");
-    res.json(result.rows);
-  } catch (error) {
-    console.error("Error fetching rooms:", error);
-    res.status(500).json({ error: "Failed to fetch rooms" });
-  }
-});
+// API Routes
 
-app.get("/messages", authenticateToken, async (req, res) => {
-  const { sender_id, recipient_id, room_id } = req.query;
+app.get("/messages", authenticateToken, db.getMessages);
+app.get("/rooms", authenticateToken, db.getRooms);
+app.post("/rooms", authenticateToken, db.createRoom);
+app.get("/users", authenticateToken, db.getUsers);
+app.post("/users", db.createUsers);
+app.post("/login", db.loginUser);
 
-  let query = "";
-  let params = [];
-
-  if (recipient_id) {
-    query = ` 
-      SELECT * 
-      FROM messages 
-      WHERE (sender_id = $1 AND recipient_id = $2)
-         OR (sender_id = $2 AND recipient_id = $1)
-      ORDER BY created_at ASC`;
-    params = [sender_id, recipient_id];
-  } else if (room_id) {
-    query = `
-      SELECT * 
-      FROM messages 
-      WHERE room_id = $1 
-      ORDER BY created_at ASC`;
-    params = [room_id];
-  } else {
-    return res.status(400).json({ error: "Missing required parameters" });
-  }
-
-  try {
-    const result = await pool.query(query, params);
-    res.json(result.rows);
-  } catch (error) {
-    console.error("Error fetching messages:", error);
-    res.status(500).json({ error: "Failed to fetch messages" });
-  }
-});
-
-app.post("/rooms", authenticateToken, async (req, res) => {
-  const { name, sender_id } = req.body;
-  try {
-    const result = await pool.query(
-      "INSERT INTO rooms (name, creator_id, joiners) VALUES ($1, $2, $3) RETURNING *",
-      [name, sender_id, [sender_id]]
-    );
-    const room = result.rows[0];
-    io.emit("new_room", room);
-    res.json(room);
-  } catch (error) {
-    console.error("Error creating room:", error);
-    res.status(500).json({ error: "Failed to create room" });
-  }
-});
-
-app.get("/users", authenticateToken, async (req, res) => {
-  try {
-    let query = "SELECT * FROM users";
-
-    const result = await pool.query(query);
-
-    res.json(result.rows);
-  } catch (error) {
-    console.error("Error fetching users:", error);
-    res.status(500).json({ error: "Failed to fetch users" });
-  }
-});
-
-app.post("/users", async (req, res) => {
-  const { userId, name, email, password, socketId } = req.body;
-  try {
-    let query = "";
-    let result;
-    if (userId) {
-      query = `UPDATE users 
-        SET socket_id = $2 
-        WHERE id = $1 
-        RETURNING id, name, email, socket_id`;
-      result = await pool.query(query, [userId, socketId]);
-      return res.status(200).json({ user: result?.rows[0] });
-    }
-    const emailCheckQuery = "SELECT * FROM users WHERE email = $1";
-    const emailCheckResult = await pool.query(emailCheckQuery, [email]);
-
-    if (emailCheckResult?.rows?.length > 0) {
-      return res
-        .status(400)
-        .json({ error: "User with this email already exists" });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    query = `
-      INSERT INTO users (name, email, password, socket_id) 
-      VALUES ($1, $2, $3, $4) 
-      RETURNING id, name, email, socket_id`;
-    result = await pool.query(query, [name, email, hashedPassword, socketId]);
-
-    const user = result.rows[0];
-
-    io.emit("new_user", user);
-    res.status(201).json({ message: "User Registered Succesfully!!" });
-  } catch (error) {
-    console.error("Error registering user:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
-
-app.post("/login", async (req, res) => {
-  const { email, password, socketId } = req.body;
-  try {
-    const query = "SELECT * FROM users WHERE email = $1";
-    const result = await pool.query(query, [email]);
-
-    if (result.rows.length === 0) {
-      return res.status(404).json({ error: "Email not found" });
-    }
-
-    const user = result.rows[0];
-
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      return res.status(401).json({ error: "Authorization failed" });
-    }
-
-    if (socketId) {
-      const updateSocketQuery = `
-        UPDATE users
-        SET socket_id = $1
-        WHERE id = $2
-        RETURNING id, name, email, socket_id`;
-      const updateSocketResult = await pool.query(updateSocketQuery, [
-        socketId,
-        user.id,
-      ]);
-
-      user.socket_id = updateSocketResult.rows[0].socket_id;
-    }
-
-    const token = jwt.sign({ id: user.id, email: user.email }, JWT_SECRET, {
-      expiresIn: "1w",
-    });
-
-    const { password: _, ...userWithoutPassword } = user;
-    res.json({ user: userWithoutPassword, token });
-  } catch (error) {
-    console.error("Error logging in user:", error);
-    res.status(500).json({ error: "Internal server error" });
-  }
-});
+// Socket Connection
 
 io.on("connection", (socket) => {
   socket.on("join_room", async ({ roomId, userId }) => {
@@ -235,6 +82,7 @@ io.on("connection", (socket) => {
       console.error("Error joining room:", error);
     }
   });
+
   socket.on(
     "send_private_message",
     async ({ sender_id, sender_info, recipient_id, content }) => {
@@ -265,7 +113,7 @@ io.on("connection", (socket) => {
     "send_room_message",
     async ({ sender_id, room_id, content, sender_info }) => {
       try {
-        io.to(room_id).emit("receive_room_message", {
+        io.in(room_id).emit("receive_room_message", {
           sender_id,
           content,
           sender_info,
